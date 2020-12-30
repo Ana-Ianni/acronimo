157675e2d82f24497f2c5ed0145c47d3f64798a4
Ana Ianni <70654287+Ana-Ianni@users.noreply.github.com>
2020-12-30 19:59:49 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -35,13 +35,11 @@ $(window).on("load",function() {
     $(window).scroll(function() {
       var windowBottom = $(this).scrollTop() + $(this).innerHeight();
       $(".fade").each(function() {
-        /* Check the location of each desired element */
         var objectBottom = $(this).offset().top + $(this).outerHeight();
         
-        /* If the element is completely within bounds of the window, fade it in */
-        if (objectBottom < windowBottom) { //object comes into view (scrolling down)
-          if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
+        if (objectBottom < windowBottom) {
+          if ($(this).css("opacity")==0) {$(this).fadeIn(1000);}
         }
       });
-    }).scroll(); //invoke scroll-handler on page-load
+    }).scroll();
   });
